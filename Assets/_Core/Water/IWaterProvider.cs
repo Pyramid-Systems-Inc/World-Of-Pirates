afc827917cd43d4b14bdfbf0dd7725e212c655b5
Omar Aglan <omar.aglan91@yahoo.com>
2025-10-18 17:57:27 +0300

    Refactor and relocate water system to Ocean folder
    Removed the old water system from _Core/Water, including scripts, shaders, and materials. Reorganized related assets and meta files into a new Ocean folder structure, and updated the Water scene to remove references to the old system. Added new URP Ocean renderer and settings assets.

--- Assets/_Core/Water/IWaterProvider.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public interface IWaterProvider
-{
-    // Returns true if sampled successfully
-    bool SampleHeightAndNormal(Vector3 worldPos, out float height, out Vector3 normal);
-    float GetSeaLevel();
-}
